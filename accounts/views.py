654f0ce65f85088216601f93e070314939482143
unknown <ciidcade143@gmail.com>
2023-08-15 12:11:45 -0700

    user registartion

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import UserForm
+from .models import User
 
 # Create your views here.
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            # read password field from  form
+            password = form.cleaned_data['password']
+            user = form.save(commit=False)
+            user.set_password(password)
+            user.role = User.CUSTOMER
+            user.save()
+            return redirect('register')
+        else:
+            print("Invalid username or password")
+            print(form.errors)
+
+    form = UserForm()
+    contex = {
+        'form': form,
+        'errors': form.errors
+
+    }
+    return render(request, 'accounts/registerUser.html', contex)
