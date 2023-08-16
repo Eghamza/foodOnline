6ea9c689d5afc7ca5fe157269a6a85132577dd2d
unknown <ciidcade143@gmail.com>
2023-08-16 13:11:56 -0700

    vendor ragistration

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect, render
 
 from vendor.forms import vendonForm
 from .forms import UserForm
-from .models import User
+from .models import User, UserProfile
 from django.contrib import messages
 
 
@@ -31,11 +31,36 @@ def register(request):
 
 
 def registervendor(request):
-    form = UserForm()
-    v_form = vendonForm()
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        v_form = vendonForm(request.POST, request.FILES)
+        
+        if form.is_valid() and v_form.is_valid:
+        
+            password = form.cleaned_data['password']
+            user = form.save(commit=False)
+            user.set_password(password)  # make the password Hash
+            user.role = User.RESTAURENT
+            user.save()
+
+            vendor_username = v_form.save(commit=False)
+            vendor_username.user = user #get the created user
+            vendor_user_profile = UserProfile.objects.get(user=user) #get the created user profile
+            vendor_username. user_profile = vendor_user_profile
+            vendor_username.save()
+            messages.success(
+                request, "User registration successful ! Please wait approving")
+            return redirect('registervendor')
+        else:
+            print("invali")
+
+    else:
+
+        form = UserForm()
+        v_form = vendonForm()
 
     contex = {
         'form': form,
         'v_form': v_form
     }
-    return render(request, 'accounts/registerVendor.html',contex)
+    return render(request, 'accounts/registerVendor.html', contex)
--- vendor/admin.py
+++ vendor/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import Vendor
 
 # Register your models here.
-admin.site.register(Vendor)
+
+
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'vendor_name', 'is_approved', 'created_at')
+
+
+admin.site.register(Vendor, VendorAdmin)
