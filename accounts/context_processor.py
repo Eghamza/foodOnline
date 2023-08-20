7e2b3c6c5e30427db60bbad069fba03c2070ab53
unknown <ciidcade143@gmail.com>
2023-08-20 12:13:52 -0700

    profile image,cover image,context processor

---
+++ accounts/context_processor.py
@@ -0,0 +1,11 @@
+from vendor import models
+
+
+def get_vendor(request):
+    try:
+     vendor = models.Vendor.objects.get(user=request.user)
+    except:
+       vendor = None
+
+
+    return dict(vendor=vendor)
--- accounts/models.py
+++ accounts/models.py
@@ -1,20 +1,22 @@
 from django.db import models
 
-from django.contrib.auth.models import AbstractBaseUser,BaseUserManager,PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 # Create your models here.
 
-#userManager
+# userManager
+
+
 class UserManager(BaseUserManager):
-    def create_user(self, username, email, password=None,first_name=None, last_name=None):
+    def create_user(self, username, email, password=None, first_name=None, last_name=None):
         if username is None:
             raise ValueError("username must not be None")
         if email is None:
             raise ValueError("email must not be None")
         user = self.model(
-            email = self.normalize_email(email),
-            username = username,
-            first_name = first_name,
-            last_name = last_name,
+            email=self.normalize_email(email),
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
 
 
         )
@@ -22,13 +24,13 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, email, password=None,first_name=None, last_name=None):
+    def create_superuser(self, username, email, password=None, first_name=None, last_name=None):
         user = self.create_user(
             email=self.normalize_email(email),
-            username = username,
-            password = password,
-            first_name = first_name,
-            last_name = last_name,
+            username=username,
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
         )
         user.is_superadmin = True
         user.is_active = True
@@ -37,28 +39,31 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-#user
+# user
+
+
 class User(AbstractBaseUser):
     RESTAURENT = 1
     CUSTOMER = 2
-    ROLE_CHOICE= (
+    ROLE_CHOICE = (
 
-        (RESTAURENT,'restaurent'),
-        (CUSTOMER,'customer'),
+        (RESTAURENT, 'restaurent'),
+        (CUSTOMER, 'customer'),
     )
     first_name = models.fields.CharField(max_length=50)
     last_name = models.fields.CharField(max_length=50)
-    username = models.fields.CharField(max_length=50,unique=True)
-    email = models.fields.CharField(max_length=50,unique=True)
-    phone_number = models.fields.CharField(max_length=12,blank=True)
-    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICE, blank=True, null=True)
+    username = models.fields.CharField(max_length=50, unique=True)
+    email = models.fields.CharField(max_length=50, unique=True)
+    phone_number = models.fields.CharField(max_length=12, blank=True)
+    role = models.PositiveSmallIntegerField(
+        choices=ROLE_CHOICE, blank=True, null=True)
 
-    #requerment
+    # requerment
     date_join = models.DateTimeField(auto_now_add=True)
     last_login = models.DateTimeField(auto_now_add=True)
     create_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now_add=True)
-    
+
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
     is_superadmin = models.BooleanField(default=False)
@@ -71,38 +76,42 @@ class User(AbstractBaseUser):
 
     def __str__(self):
         return self.email
-    
-    def has_perm(self,perm, obj=None):
+
+    def has_perm(self, perm, obj=None):
         return self.is_admin
-    
-    def has_module_perms(self,app_label):
+
+    def has_module_perms(self, app_label):
         return True
-    
-    #get user role
+
+    # get user role
     def get_role(self):
         if self.role == 1:
             user_role = 'RESTAURENT'
             return user_role
         if self.role == 2:
-           user_role = 'CUSTOMER'   
-           return user_role 
+            user_role = 'CUSTOMER'
+            return user_role
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User,models.CASCADE,blank=True)
-    profile_picture = models.ImageField(upload_to='user/profile_picture', blank=True, null=True)
-    cover_photo = models.ImageField(upload_to='user/cover_photo', blank=True, null=True)
+    user = models.OneToOneField(User, models.CASCADE, blank=True)
+    profile_picture = models.ImageField(
+        upload_to='user/profile_picture', blank=True, null=True)
+    cover_photo = models.ImageField(
+        upload_to='user/cover_photo', blank=True, null=True)
     address_line_1 = models.CharField(max_length=50, blank=True, null=True)
     address_line_2 = models.CharField(max_length=50, blank=True, null=True)
     country = models.CharField(max_length=15, blank=True, null=True)
     state = models.CharField(max_length=15, blank=True, null=True)
-    city =models.CharField(max_length=15, blank=True, null=True)
-    pin_code = models.CharField(max_length=6,blank=True, null=True)
+    city = models.CharField(max_length=15, blank=True, null=True)
+    pin_code = models.CharField(max_length=6, blank=True, null=True)
     latitude = models.CharField(max_length=15, blank=True, null=True)
     longtitude = models.CharField(max_length=15, blank=True, null=True)
-    create_at =models.DateTimeField(auto_now_add=True)
-    modified_at =models.DateTimeField(auto_now=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
 
-def __str__(self):
-    self.user.email
+    def full_address(self):
+        return f'{self.address_line_1} {self.address_line_2}'
 
+    def __str__(self):
+        self.user.email
