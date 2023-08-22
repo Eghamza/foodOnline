705e56b161eed60e7af34ba24b864b0177d37fd3
unknown <ciidcade143@gmail.com>
2023-08-22 14:17:44 -0700

    fixing user profile

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,24 +7,32 @@ from .validators import allow_only_image_validators
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
     c_password = forms.CharField(widget=forms.PasswordInput())
+
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username',
                   'email', 'phone_number', 'password']
-        
+
     def clean(self) -> Dict[str, Any]:
-        data_cleaned = super(UserForm,self).clean()
-        password = data_cleaned.get("password")     
+        data_cleaned = super(UserForm, self).clean()
+        password = data_cleaned.get("password")
         c_paswd = data_cleaned.get('c_password')
 
         if c_paswd != password:
             raise forms.ValidationError("conform password not match!")
 
 
-
 class UserProfileForm(forms.ModelForm):
-    profile_picture = forms.ImageField(widget=forms.FileInput(attrs={'class':'btn btn-info'}), validators=[allow_only_image_validators]) #css
-    cover_photo = forms.ImageField(widget=forms.FileInput(attrs={'class':'btn btn-info' }),validators=[allow_only_image_validators])
+    profile_picture = forms.FileField(widget=forms.FileInput(
+        attrs={'class': 'btn btn-info'}), validators=[allow_only_image_validators])  # css
+    cover_photo = forms.FileField(widget=forms.FileInput(
+        attrs={'class': 'btn btn-info'}), validators=[allow_only_image_validators])
+    latitude = forms.CharField(
+        widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    longtitude = forms.CharField(
+        widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+
     class Meta:
         model = UserProfile
-        fields = ['profile_picture', 'cover_photo', 'address_line_1','address_line_2','country','state','city','pin_code','latitude','longtitude']
+        fields = ['profile_picture', 'cover_photo', 'address_line_1', 'address_line_2',
+                  'country', 'state', 'city', 'pin_code', 'latitude', 'longtitude']
--- vendor/views.py
+++ vendor/views.py
@@ -1,39 +1,41 @@
-from django.shortcuts import render,get_object_or_404,redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from accounts.forms import UserProfileForm
 from accounts.models import UserProfile
 from .models import Vendor
-from  .forms import vendonForm
+from .forms import vendonForm
 from django.contrib import messages
 # Create your views here.
 
 
 def profile(request):
-    user_profile = get_object_or_404(UserProfile,user = request.user)
-    
-    vender_instance = get_object_or_404(Vendor, user = request.user)
-    
-    #update 
+    # get user data
+    user_profile = get_object_or_404(UserProfile, user=request.user)
+    vender_instance = get_object_or_404(Vendor, user=request.user)
+
+    # update
     if request.method == 'POST':
-        user_p = UserProfileForm(request.POST,request.FILES,instance=user_profile)
-        vendor =vendonForm(request.POST,request.FILES,instance=vender_instance)
+        user_p = UserProfileForm(
+            request.POST, request.FILES, instance=user_profile)
+        vendor = vendonForm(request.POST, request.FILES,
+                            instance=vender_instance)
         if user_p.is_valid() and vendor.is_valid():
             user_p.save()
             vendor.save()
-            messages.success(request,"Profile updated")
+            messages.success(request, "Profile updated")
             return redirect('vprofile')
         else:
-           print("--------------------",user_p.errors)
-           print("--------------------",vendor.errors)
-    
-    else:
-        print("-------------------- hello world")
-        vendon = vendonForm(instance=vender_instance)
-        p = UserProfileForm(instance = user_profile)
+            print("--------------------", user_p.errors)
+            print("--------------------", vendor.errors)
 
-    context ={
-        'profile': p,
-        'vendorform': vendon,
-        'user_p':user_profile,
+    else:
+
+        vendor = vendonForm(instance=vender_instance)
+        user_p = UserProfileForm(instance=user_profile)
+
+    context = {
+        'profile': user_p,
+        'vendorform': vendor,
+        'user_p': user_profile,
         'vender_instance': vender_instance,
     }
-    return render(request, 'vendor/vprofile.html',context)
+    return render(request, 'vendor/vprofile.html', context)
